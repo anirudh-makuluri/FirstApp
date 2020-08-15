4245bc395d03648ebd3a7b03b029721e20f813c5
armitron1708 <39943846+armitron1708@users.noreply.github.com>
2020-08-15 19:28:03 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -21,7 +21,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
-      drawer: NavDrawer(),
+     // drawer: NavDrawer(),
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Colors.black45,
